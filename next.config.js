5500f550caa52dea2411c6bf2fd1c35f6fe433dd
Akash Shukla <akashshukla0855@gmail.com>
2024-02-25 11:01:44 +0530

    setup

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["lumiere-a.akamaihd.net", "c4.wallpaperflare.com"], // Add the hostname here
+  },
+};
+
+module.exports = nextConfig;
